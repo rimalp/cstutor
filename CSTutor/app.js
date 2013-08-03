6ef309d24c8ff0e034d2fb5ab98a4ad47ec4b795
Prabhat <pravat.rimal@gmail.com>
2013-08-03 01:12:02 -0400

    changes to db.js

--- CSTutor/app.js
+++ CSTutor/app.js
@@ -47,6 +47,173 @@ if ('development' == app.get('env')) {
 
 app.get('/', routes.index); //home page
 
+
+
+
+
+
+// more about routes
+	//getting simple requests
+	// app.get("/", function(req, res) {
+	// 	res.send("Hello, Express!"); //can directly type html here
+	// });
+
+	//getting get requests with params
+	// app.get("/users/:userId", function(req, res){
+	// 	res.send("<h1> Hello, user " + req.params.userId + " ! </h1>");
+	// });
+
+	//getting post requests
+	// app.post("/users", function(req, res){
+	// 	res.send("Creating a new user with name: " + req.body.username); //username is a param in the form
+	// });
+
+	//same with put
+	// app.put("/users/:userId", function(){})
+	// app.delete("/users/:userId. function(){}");
+
+	//can also use regex in routes
+
+		// app.get(/\/users\/(\d*)\/?(edit)?/, function(req, res){
+		// 	// /users/10 (number)
+		// 	// /users/10/
+		// 	// /users/10/edit
+
+		// 	var message = "user #" + req.params[0] + "'s profile.";
+		// 	if(req.params[1] == 'edit') {
+		// 		message = "Editing " + message;
+		// 	} else {
+		// 		message = "Viewing " + message;
+		// 	}
+
+		// 	res.send(message);
+		// });
+
+
+// //rendering views
+// 	app.get("/", function(req, res){
+// 		res.render("abc.jade")
+// 	});
+
+http.createServer(app).listen(app.get('port'), function(){
+  console.log('Express server listening on port ' + app.get('port'));
+});
+
+
+
+//Database connection
+// var client = new pg.Client(conString);
+console.log("before");
+var client = new pg.Client({user: 'rimalp', database: 'gfb', host: 'ww2.cs.lafayette.edu', port: 5432 });
+console.log("after");
+client.connect(function(err) {
+	console.log("connect called");
+  	if(err) {
+   		return console.error('could not connect to postgres: ', err);
+  	}
+	console.log("connected");
+  	//create all the tables here if not exist
+	var create_Student = "CREATE TABLE student(email varchar PRIMARY KEY, firstName varchar, lastName varchar, password varchar, frequency integer)";
+	var create_Prof = "CREATE TABLE professor(email varchar PRIMARY KEY, firstName varchar, lastName varchar, password varchar)";
+	var create_Course = "CREATE TABLE course(name varchar, year integer, semester varchar, PRIMARY KEY(name, year, semester))";
+	var create_ProfCourse = "CREATE TABLE  professor_course(email varchar, courseName varchar, courseYear integer, courseSemester varchar, PRIMARY KEY(email, courseName, courseYear, courseSemester))";
+	var create_StudentCourse = "CREATE TABLE  student_course(email varchar, courseName varchar, courseYear integer, courseSemester varchar, PRIMARY KEY(email, courseName, courseYear, courseSemester))";
+	var create_Project = "CREATE TABLE  project(name text, description text, dueDate DATE, courseName varchar, courseYear integer, courseSemester varchar, PRIMARY KEY(courseName, courseYear, courseSemester, name))";
+	var create_StudentProject = "CREATE TABLE  student_project(projectName text, courseName varchar, courseYear integer, courseSemester varchar, email varchar, graphId integer, PRIMARY KEY(projectName, courseName, courseYear, courseSemester, email, graphId))";
+	var create_Node = "CREATE TABLE  node(id SERIAL, x integer, y integer, graphId integer, parentNodeId integer, name integer, description integer, color varchar)";
+	var create_Edge = "CREATE TABLE  edge (src integer, dst integer, graphId integer, PRIMARY KEY(src, dst))";
+	var create_Graph = "CREATE TABLE  graph(id SERIAL, parentNodeId integer UNIQUE, version integer, description text)";
+
+	var create_Question = "CREATE TABLE  question(id SERIAL, projectId integer, question varchar)";
+	var create_Answer = "CREATE TABLE  answer(id SERIAL, questionId integer, graphId integer, answer varchar)";
+
+  client.query('SELECT NOW() AS "theTime"', function(err, result) {
+    if(err) {
+      return console.error('error running a query.', err);
+    }
+    console.log(result.rows[0].theTime);
+    //output: Tue Jan 15 2013 19:12:47 GMT-600 (CST)
+  });
+
+  client.query("drop schema public cascade", function(err){
+  	if(!err){
+  		client.query("create schema public", function(err){
+  			if(err){
+  				console.log("Error: " + err);
+  			}else{
+  				//create table queries execution
+			    client.query(create_Student, function(err, result) {
+				    if(err) {
+				      return console.error('error running a create student query.', err);
+				    }
+			  	});
+
+			    client.query(create_Prof, function(err, result) {
+				    if(err) {
+				      return console.error('error running a CREATE PROFESSOR query.', err);
+				    }
+			  	});
+
+			    client.query(create_Course, function(err, result) {
+				    if(err) {
+				      return console.error('error running a create COURSE query.', err);
+				    }
+			  	});
+			    client.query(create_ProfCourse, function(err, result) {
+				    if(err) {
+				      return console.error('error running a query.', err);
+				    }
+			  	});
+			    client.query(create_StudentCourse, function(err, result) {
+				    if(err) {
+				      return console.error('error running a create STUDENT_COURSE query.', err);
+				    }
+			  	});
+			    client.query(create_Project, function(err, result) {
+				    if(err) {
+				      return console.error('error running a create PROJECT query.', err);
+				    }
+			  	});
+			    client.query(create_StudentProject, function(err, result) {
+				    if(err) {
+				      return console.error('error running a create STUDENT_PROJECT query.', err);
+				    }
+			  	});
+			    client.query(create_Node, function(err, result) {
+				    if(err) {
+				      return console.error('error running a create NODE query.', err);
+				    }
+			  	});
+			    client.query(create_Edge, function(err, result) {
+				    if(err) {
+				      return console.error('error running a create EDGE query.', err);
+				    }
+			  	});
+			    client.query(create_Graph, function(err, result) {
+				    if(err) {
+				      return console.error('error running a create GRAPH query.', err);
+				    }
+			  	});
+
+			    client.on('drain', function(){
+			    	console.log("FINISHED TABLE CREATION.");
+			    });
+
+  			}
+
+  		});
+  	}
+  });
+    });
+
+var database = new db.Database(client);
+
+
+
+console.log("database object created");
+
+
+
 //==============  GET requests for database queries ===============================
 var sendPostResponse = function(req, res, err, result){
 	if(err){
@@ -54,12 +221,13 @@ var sendPostResponse = function(req, res, err, result){
 			res.end("Error reading the database");
 		}else{
 			res.writeHead(200, { 'Content-Type': 'text/plain' });
-			res.end(JSON.stringify(test));
+			res.end(JSON.stringify(result.rows));
 		}
 }
 //get all the courses for a student. request body params:  studentEmail
 app.post('/courses_student', function(req, res){
-	database.getCoursesForStudent(req.body.studentEmail, function(err, result){
+	console.log("get students request: " + req.body.email);
+	database.getCoursesForStudent(req.body.email, function(err, result){
 		sendPostResponse(req, res, err, result);
 	});
 });
@@ -144,153 +312,3 @@ app.put('/create_graph', function(req, res){
 app.put('/update_graph', function(req, res){
 
 });
-
-
-
-
-// more about routes
-	//getting simple requests
-	// app.get("/", function(req, res) {
-	// 	res.send("Hello, Express!"); //can directly type html here
-	// });
-
-	//getting get requests with params
-	// app.get("/users/:userId", function(req, res){
-	// 	res.send("<h1> Hello, user " + req.params.userId + " ! </h1>");
-	// });
-
-	//getting post requests
-	// app.post("/users", function(req, res){
-	// 	res.send("Creating a new user with name: " + req.body.username); //username is a param in the form
-	// });
-
-	//same with put
-	// app.put("/users/:userId", function(){})
-	// app.delete("/users/:userId. function(){}");
-
-	//can also use regex in routes
-
-		// app.get(/\/users\/(\d*)\/?(edit)?/, function(req, res){
-		// 	// /users/10 (number)
-		// 	// /users/10/
-		// 	// /users/10/edit
-
-		// 	var message = "user #" + req.params[0] + "'s profile.";
-		// 	if(req.params[1] == 'edit') {
-		// 		message = "Editing " + message;
-		// 	} else {
-		// 		message = "Viewing " + message;
-		// 	}
-
-		// 	res.send(message);
-		// });
-
-
-// //rendering views
-// 	app.get("/", function(req, res){
-// 		res.render("abc.jade")
-// 	});
-
-http.createServer(app).listen(app.get('port'), function(){
-  console.log('Express server listening on port ' + app.get('port'));
-});
-
-
-
-//Database connection
-// var client = new pg.Client(conString);
-console.log("before");
-var client = new pg.Client({user: 'rimalp', password: 'rimalp', database: 'gfb', host: 'localhost', port: 5432 });
-console.log("after");
-client.connect(function(err) {
-	console.log("connect called");
-  	if(err) {
-   		return console.error('could not connect to postgres: ', err);
-  	}
-	console.log("connected");
-  	//create all the tables here if not exist
-	var create_Student = "CREATE TABLE IF NOT EXISTS student(email varchar PRIMARY KEY, firstName varchar, lastName varchar, password varchar, int frequency)";
-	var create_Prof = "CREATE TABLE IF NOT EXISTS professor(email varchar PRIMARY KEY, firstName varchar, lastName varchar, password varchar)";
-	var create_Course = "CREATE TABLE IF NOT EXISTS course(name varchar, year integer, semester varchar, PRIMARY KEY(name, year, semester))";
-	var create_ProfCourse = "CREATE TABLE IF NOT EXISTS professor_course(email varchar, courseName varchar, courseYear integer, courseSemester varchar, PRIMARY KEY(email, courseName, courseYear, courseSemester))";
-	var create_StudentCourse = "CREATE TABLE IF NOT EXISTS student_course(email varchar, courseName varchar, courseYear integer, courseSemester varchar, PRIMARY KEY(email, courseName, courseYear, semester))";
-	var create_Project = "CREATE TABLE IF NOT EXISTS project(name text, description text, dueDate DATE, courseName varchar, courseYear integer, courseSemester varchar, PRIMARY KEY(courseName, courseYear, courseSemester, name))";
-	var create_StudentProject = "CREATE TABLE IF NOT EXISTS student_project(projectName text, courseName varchar, courseYear integer, courseSemester varchar, email varchar, graphId integer, PRIMARY KEY(projectName, courseName, courseYear, courseSemester, email, graphId))";
-	var create_Node = "CREATE TABLE IF NOT EXISTS node(id SERIAL, x integer, y integer, graphId integer, parentNodeId integer, name integer, description integer, color varchar)";
-	var create_Edge = "CREATE TABLE IF NOT EXISTS edge (src integer, dst integer, graphId integer, PRIMARY KEY(src, dst))";
-	var create_Graph = "CREATE TABLE IF NOT EXISTS graph(id SERIAL, parentNodeId integer UNIQUE, version integer, description text)";
-
-	var create_Question = "CREATE TABLE IF NOT EXISTS question(id SERIAL, projectId integer, question varchar)";
-	var create_Answer = "CREATE TABLE IF NOT EXISTS answer(id SERIAL, questionId integer, graphId integer, answer varchar)";
-
-  client.query('SELECT NOW() AS "theTime"', function(err, result) {
-    if(err) {
-      return console.error('error running a query.', err);
-    }
-    console.log(result.rows[0].theTime);
-    //output: Tue Jan 15 2013 19:12:47 GMT-600 (CST)
-  });
-
-    //create table queries execution
-    client.query(create_Student, function(err, result) {
-	    if(err) {
-	      return console.error('error running a create student query.', err);
-	    }
-  	});
-
-    client.query(create_Prof, function(err, result) {
-	    if(err) {
-	      return console.error('error running a CREATE PROFESSOR query.', err);
-	    }
-  	});
-
-    client.query(create_Course, function(err, result) {
-	    if(err) {
-	      return console.error('error running a create COURSE query.', err);
-	    }
-  	});
-    client.query(create_ProfCourse, function(err, result) {
-	    if(err) {
-	      return console.error('error running a query.', err);
-	    }
-  	});
-    client.query(create_StudentCourse, function(err, result) {
-	    if(err) {
-	      return console.error('error running a create STUDENT_COURSE query.', err);
-	    }
-  	});
-    client.query(create_Project, function(err, result) {
-	    if(err) {
-	      return console.error('error running a create PROJECT query.', err);
-	    }
-  	});
-    client.query(create_StudentProject, function(err, result) {
-	    if(err) {
-	      return console.error('error running a create STUDENT_PROJECT query.', err);
-	    }
-  	});
-    client.query(create_Node, function(err, result) {
-	    if(err) {
-	      return console.error('error running a create NODE query.', err);
-	    }
-  	});
-    client.query(create_Edge, function(err, result) {
-	    if(err) {
-	      return console.error('error running a create EDGE query.', err);
-	    }
-  	});
-    client.query(create_Graph, function(err, result) {
-	    if(err) {
-	      return console.error('error running a create GRAPH query.', err);
-	    }
-  	});
-
-    client.on('drain', function(){
-    	console.log("FINISHED TABLE CREATION.");
-    });
-});
-
-exports.database = db.Database(client);
-
-
-console.log("database object created");
